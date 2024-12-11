f4d53f24b2139d0aea21f52120e79ea20f25f89a
vijayanborkar <vijayanborkar27@gmail.com>
2024-12-11 12:39:49 +0530

    feat: frontend

---
+++ cloudinary-frontend/src/constants/imageUpload.js
@@ -0,0 +1,3 @@
+export const FILE_UPLOAD_URL = "http://localhost:4040/files/upload";
+export const JWT_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsZXNzb24iOiJjbG91ZGluYXJ5VXBsb2FkIiwiaWF0IjoxNzMzNDY0Mjc4LCJleHAiOjE3MzM0Njc4Nzh9.7_k4Ro70iNPegOFrk53F-UN7pjmtlvLYZWwyPVxhtfw";
